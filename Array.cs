06d211a11bc4017e129b50e2409798bcf966373f
khoa12387 <100249459+khoa12387@users.noreply.github.com>
2024-10-25 16:13:11 +0700

    Add files via upload
    2 Bài tập về Constructor và Array

---
+++ Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__CanBan
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Nhap so thu " + (i + 1) + ": ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Cac so chan trong mang la :");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+        }
+    }
+}
