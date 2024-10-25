d00d7da8af49425885fb5ceb427d263572554d95
khoa12387 <100249459+khoa12387@users.noreply.github.com>
2024-10-25 16:18:53 +0700

    Add files via upload
    2 bài tập về Constructor và Array C#

---
+++ C#_CanBan/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__CanBan
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public string Address;
+
+        public Person(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Address: " + Address);
+        }
+    }
+
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Person person = new Person("Khoa", 21, "149 Thanh My Loi");
+            person.DisplayInfo();
+        }
+    }
+}
